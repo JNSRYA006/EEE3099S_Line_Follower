e5a83507a4bcc2377bdd7d667818fa37c3099fa5
Ryan Jones <JSNRYA006@myuct.ac.za>
2022-08-17 10:32:18 +0200

    Update robot parameters

--- Simulation/MATLab & Simulink Files/robotParameters.m
+++ Simulation/MATLab & Simulink Files/robotParameters.m
@@ -2,7 +2,7 @@
 wheelR = 0.031; %Wheel radius (m)
 axleLength = 0.136; %Distance between wheels (m)
 ticksPerRot = 20; % Ticks per rotation for encoders
-load wheelLUT %Look up table for platform motors
+load wheelLUT.mat %Look up table for platform motors
 Ts = 0.01; %Sample time
-load wheelMotorModel %Motor model for platform motorswheelMotorModel
-load straigh_line.mat
+load wheelMotorModel.mat %Motor model for platform motorswheelMotorModel
+load straight_line.mat
